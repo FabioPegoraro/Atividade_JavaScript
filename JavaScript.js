170812246bdfa5578caeae6d0e9cbd2f58a7a943
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-17 22:20:01 -0300

    Atividade (Qual numero é maior)

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+let Numero1 = parseInt(window.prompt("Digite um número"));
+
+let Numero2 = parseInt(window.prompt("Digite um número"));
+
+if(Numero1 > Numero2){
+    alert("O número " + Numero1 + " é maior")
+} else {
+    alert("O número " + Numero2 + " é maior")
+}
+
+
+
